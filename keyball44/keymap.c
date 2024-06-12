472b939e9c1077052879fdb4aae1e62614c49f49
tomita <you.tomi02@gmail.com>
2024-06-12 13:43:14 +0900

    20240612

--- keyball44/keymap.c
+++ keyball44/keymap.c
@@ -31,30 +31,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for default (VIA)
   [0] = LAYOUT_universal(
-    KC_ESC        , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                   KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC   ,
-    LCTL_T(KC_TAB), KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                   KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , LT(1,KC_ENT)  ,
-    KC_LSFT       , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                   KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , LT(2,KC_ENT)  ,
+    KC_ESC        , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                   KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_GRV        ,
+    LCTL_T(KC_TAB), KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                   KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , LT(1,KC_QUOT) ,
+    KC_LSFT       , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                   KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , LT(2,KC_RSFT) ,
                                KC_LALT ,KC_LGUI,      LT(4, KC_LNG1),KC_SPC ,LT(3,KC_LNG2),     KC_BSPC   , KC_ENT,           RCTL_T(KC_LNG2), KC_RALT , KC_RALT
   ),
 
   [1] = LAYOUT_universal(
-    KC_F1         , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    ,                                   KC_F7    , KC_F8    , KC_F9    , KC_F10  , KC_F11   , KC_F12   ,
-    _______       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , KC_BTN1  , KC_BTN2  , KC_RCTL , KC_RSFT  , _______  ,
+    _______       , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                                   KC_F6    , KC_F7    , KC_F8    , KC_F9   , KC_F10   , _______  ,
+    _______       , KC_F11   , KC_F12   , _______  , _______  , _______  ,                                   _______  , KC_BTN1  , KC_BTN2  , KC_RCTL , KC_RSFT  , _______  ,
     _______       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , KC_BTN3  , _______  , _______ , _______  , _______  ,
                                _______  , _______  ,     _______  , _______  , _______  ,       KC_BTN4  , KC_BTN5  ,         _______  , _______      , _______
   ),
 
   [2] = LAYOUT_universal(
-    _______       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , _______  , _______  , KC_UP    , _______  , KC_DEL   ,
-    KC_CAPS       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , _______  , KC_LEFT  , KC_RGHT  , _______  , _______  ,
-    _______       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , _______  , _______  , KC_DOWN  , _______  , _______  ,
+    KC_CAPS       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , _______  , _______  , KC_UP    , _______  , _______  ,
+    KC_LCTL       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , _______  , KC_LEFT  , KC_RGHT  , KC_RSFT  , _______  ,
+    KC_LSFT       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , _______  , _______  , KC_DOWN  , _______  , _______  ,
                                _______  , _______  ,     _______  , _______  , _______  ,       KC_DEL   , _______  ,         _______  , _______       , _______
   ),
 
   [3] = LAYOUT_universal(
-    KC_BSLS       , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                   KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_GRV   ,
-    _______       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , KC_MINS  , KC_EQL   , KC_LBRC  , KC_RBRC  , KC_QUOT  ,
-    _______       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , _______  , _______  , _______  , _______  , _______  ,
+    _______       , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                   KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_BSLS  ,
+    _______       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , KC_MINS  , KC_EQL   , KC_LBRC  , KC_RBRC  , _______  ,
+    KC_LSFT       , _______  , _______  , _______  , _______  , _______  ,                                   _______  , _______  , _______  , _______  , _______  , KC_RSFT  ,
                                _______  , _______  ,     _______  , _______  , _______  ,       _______  , _______  ,         _______  , _______       , _______
   ),
 
